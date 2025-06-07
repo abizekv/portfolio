435cb94537c5a7d4c55f5633b7001ea23f32133f
Abizek <abizekv@gmail.com>
2025-06-07 15:28:00 +0530

    about and contact section layout finished

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,15 @@
 export default function About(){
     return (
-        <h2>About</h2>
+        <section className="about-section">
+            <h2 className="about-section-title">
+                🧑🏻‍💻 About Me
+            </h2>
+            <hr />
+            <p className="about-section-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quos laboriosam facilis vero deserunt architecto nisi, modi dolore? Laborum autem aperiam ea dicta amet iure consequatur voluptates perspiciatis enim ratione. 
+                <br /><br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat earum tempora maiores sunt, maxime hic veritatis amet possimus consequatur quam ratione, autem aliquam consequuntur dolor voluptatem. Sequi ratione aperiam modi.     
+            </p>
+        </section>
     )
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,14 @@
-export default function Contact(){
+export default function Contact() {
     return (
-        <h2>Contact</h2>
+        <section className="contact-section">
+            <h2 className="contact-section-title">📲 Get in touch</h2>
+            <hr />
+            <p className="contact-section-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta impedit officia et dolores natus architecto minus, quia consequatur, debitis repellat expedita laudantium deserunt repudiandae ipsum, maxime quo eum doloribus quam?
+                <br /> <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quibusdam distinctio eum aut eligendi velit tenetur, ipsam error dolores? Optio similique suscipit ea? Suscipit aliquid ducimus ea nesciunt ipsum itaque.
+            </p>
+        </section>
+
     )
 }
