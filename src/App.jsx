5ce543a504b2a4742e973716ebf67c0ad8098d38
Abizek <abizekv@gmail.com>
2025-06-10 17:16:11 +0530

    add dev credit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './index.css'
 import Navbar from './Navbar'
 import ProfileCard from './ProfileCard'
 import SecondaryNavbar from './SecondaryNavbar'
+import DevCredit from './DevCredit'
 import { useState } from 'react'
 // Pages
 import Projects from './pages/Projects'
@@ -11,22 +12,23 @@ import Skills from './pages/Skills'
 function App() {
   const [activeTab, setActiveTab] = useState('projects')
 
-  function selectTab(pageName){
+  function selectTab(pageName) {
     setActiveTab(pageName)
   }
   return (
     <>
       <Navbar />
       <ProfileCard />
-      <SecondaryNavbar 
-      selectTab={selectTab}
-      activeTab={activeTab}
+      <SecondaryNavbar
+        selectTab={selectTab}
+        activeTab={activeTab}
       />
-      
+
       {activeTab === 'projects' && <Projects />}
       {activeTab === 'skills' && <Skills />}
       {activeTab === 'about' && <About />}
       {activeTab === 'contact' && <Contact />}
+      <DevCredit />
     </>
   )
 }
---
+++ src/DevCredit.jsx
@@ -0,0 +1,5 @@
+export default function DevCredit(){
+    return(      
+      <p className="dev-credit">Developed by Abhishek V</p>
+    )
+}
